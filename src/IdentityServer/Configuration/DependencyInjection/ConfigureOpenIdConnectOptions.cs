416a96f445ab897b6d744b18c29b8d8875ad48ab
Stefan Nikolei <stefan@nikolei.eu>
2021-01-05 23:27:11 +0100

    Change usage in DistributedCacheStateDataFormatter

--- src/IdentityServer/Configuration/DependencyInjection/ConfigureOpenIdConnectOptions.cs
+++ src/IdentityServer/Configuration/DependencyInjection/ConfigureOpenIdConnectOptions.cs
@@ -1,25 +1,24 @@
-﻿// Copyright (c) Duende Software. All rights reserved.
+// Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
 
-using System;
-using System.Linq;
 using Duende.IdentityServer.Infrastructure;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
 
 namespace Duende.IdentityServer.Configuration
 {
     internal class ConfigureOpenIdConnectOptions : IPostConfigureOptions<OpenIdConnectOptions>
     {
         private string[] _schemes;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IServiceProvider _serviceProvider;
 
-        public ConfigureOpenIdConnectOptions(string[] schemes, IHttpContextAccessor httpContextAccessor)
+        public ConfigureOpenIdConnectOptions(string[] schemes, IServiceProvider serviceProvider)
         {
             _schemes = schemes ?? throw new ArgumentNullException(nameof(schemes));
-            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
         }
 
         public void PostConfigure(string name, OpenIdConnectOptions options)
@@ -27,7 +26,7 @@ namespace Duende.IdentityServer.Configuration
             // no schemes means configure them all
             if (_schemes.Length == 0 || _schemes.Contains(name))
             {
-                options.StateDataFormat = new DistributedCacheStateDataFormatter(_httpContextAccessor, name);
+                options.StateDataFormat = new DistributedCacheStateDataFormatter(_serviceProvider, name);
             }
         }
     }
