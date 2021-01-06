353d9e87a735f556ae9bcd05d3ea0f6897245c06
Stefan Nikolei <stefan@nikolei.eu>
2021-01-06 15:10:37 +0100

    * Use ValueTask where appropriate * spawn Tasks and await later where appropriate * create a static instance of JsonWebTokenHandler. It does not save any instance data and thus does not need to be created every single time

--- src/IdentityServer/Services/Default/DefaultTokenCreationService.cs
+++ src/IdentityServer/Services/Default/DefaultTokenCreationService.cs
@@ -2,21 +2,16 @@
 // See LICENSE in the project root for license information.
 
 
-using IdentityModel;
+using Duende.IdentityServer.Configuration;
 using Duende.IdentityServer.Extensions;
 using Duende.IdentityServer.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.JsonWebTokens;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IdentityModel.Tokens.Jwt;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Duende.IdentityServer.Configuration;
-
-using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace Duende.IdentityServer.Services
 {
@@ -25,6 +20,8 @@ namespace Duende.IdentityServer.Services
     /// </summary>
     public class DefaultTokenCreationService : ITokenCreationService
     {
+        private static readonly JsonWebTokenHandler _handler;
+
         /// <summary>
         /// The key service
         /// </summary>
@@ -64,6 +61,11 @@ namespace Duende.IdentityServer.Services
             Logger = logger;
         }
 
+        static DefaultTokenCreationService()
+        {
+            _handler = new JsonWebTokenHandler { SetDefaultTimesOnTokenCreation = false };
+        }
+
         /// <summary>
         /// Creates the token.
         /// </summary>
@@ -73,32 +75,49 @@ namespace Duende.IdentityServer.Services
         /// </returns>
         public virtual async Task<string> CreateTokenAsync(Token token)
         {
-            var payload = await CreatePayloadAsync(token);
-            var headerElements = await CreateHeaderElementsAsync(token);
+            var payload = CreatePayloadAsync(token);
+            var headerElements = CreateHeaderElementsAsync(token);
 
-            return await CreateJwtAsync(token, payload, headerElements);
+            return await CreateJwtAsync(token, await payload, await headerElements);
         }
+
 
         /// <summary>
         /// Creates the JWT payload
         /// </summary>
         /// <param name="token"></param>
         /// <returns></returns>
+#if NET5_0
+        protected virtual ValueTask<string> CreatePayloadAsync(Token token)
+#endif
+#if NETCOREAPP3_1
         protected virtual Task<string> CreatePayloadAsync(Token token)
+#endif
         {
             var payload = token.CreateJwtPayloadDictionary(Options, Clock, Logger);
+#if NET5_0
+            return ValueTask.FromResult(JsonSerializer.Serialize(payload));
+#endif
+#if NETCOREAPP3_1
             return Task.FromResult(JsonSerializer.Serialize(payload));
+#endif
         }
+
 
         /// <summary>
         /// Creates additional JWT header elements
         /// </summary>
         /// <param name="token"></param>
         /// <returns></returns>
+#if NET5_0
+        protected virtual ValueTask<Dictionary<string, object>> CreateHeaderElementsAsync(Token token)
+#endif
+#if NETCOREAPP3_1
         protected virtual Task<Dictionary<string, object>> CreateHeaderElementsAsync(Token token)
+#endif
         {
             var additionalHeaderElements = new Dictionary<string, object>();
-            
+
             if (token.Type == IdentityServerConstants.TokenTypes.AccessToken)
             {
                 if (Options.AccessTokenJwtType.IsPresent())
@@ -106,8 +125,12 @@ namespace Duende.IdentityServer.Services
                     additionalHeaderElements.Add("typ", Options.AccessTokenJwtType);
                 }
             }
-
+#if NET5_0
+            return ValueTask.FromResult(additionalHeaderElements);
+#endif
+#if NETCOREAPP3_1
             return Task.FromResult(additionalHeaderElements);
+#endif
         }
 
         /// <summary>
@@ -128,8 +151,7 @@ namespace Duende.IdentityServer.Services
                 throw new InvalidOperationException("No signing credential is configured. Can't create JWT token");
             }
 
-            var handler = new JsonWebTokenHandler { SetDefaultTimesOnTokenCreation = false };
-            return handler.CreateToken(payload, credential, headerElements);
+            return _handler.CreateToken(payload, credential, headerElements);
         }
     }
 }
