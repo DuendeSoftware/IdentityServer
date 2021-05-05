e718d491aec44d84578aebb9bdd7366d3a9d2ce0
Brock Allen <brockallen@gmail.com>
2021-05-05 09:20:21 -0400

    always allow explicit error responses from authorize interaction response generator, regardless if consent is needed

--- src/IdentityServer/ResponseHandling/Default/AuthorizeInteractionResponseGenerator.cs
+++ src/IdentityServer/ResponseHandling/Default/AuthorizeInteractionResponseGenerator.cs
@@ -79,7 +79,7 @@ namespace Duende.IdentityServer.ResponseHandling
         {
             Logger.LogTrace("ProcessInteractionAsync");
 
-            if (consent != null && consent.Granted == false && consent.Error.HasValue && request.Subject.IsAuthenticated() == false)
+            if (consent != null && consent.Granted == false && consent.Error.HasValue)
             {
                 // special case when anonymous user has issued an error prior to authenticating
                 Logger.LogInformation("Error: User consent result: {error}", consent.Error);
