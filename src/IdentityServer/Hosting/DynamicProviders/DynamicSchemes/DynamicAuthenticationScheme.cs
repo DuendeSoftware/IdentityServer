8ab108cc8c502e86362ecbc999523494bfd7990b
Brock Allen <brockallen@gmail.com>
2021-05-18 18:06:17 -0400

    change accessibility to public

--- src/IdentityServer/Hosting/DynamicProviders/DynamicSchemes/DynamicAuthenticationScheme.cs
+++ src/IdentityServer/Hosting/DynamicProviders/DynamicSchemes/DynamicAuthenticationScheme.cs
@@ -7,14 +7,25 @@ using System;
 
 namespace Duende.IdentityServer.Hosting.DynamicProviders
 {
-    class DynamicAuthenticationScheme : AuthenticationScheme
+    /// <summary>
+    /// Models a dynamic authentication scheme and it's corresponding IdentityProvider data.
+    /// </summary>
+    public class DynamicAuthenticationScheme : AuthenticationScheme
     {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="idp"></param>
+        /// <param name="handlerType"></param>
         public DynamicAuthenticationScheme(IdentityProvider idp, Type handlerType)
             : base(idp.Scheme, idp.DisplayName, handlerType)
         {
             IdentityProvider = idp;
         }
 
+        /// <summary>
+        /// The corresponding IdentityProvider configuration data.
+        /// </summary>
         public IdentityProvider IdentityProvider { get; set; }
     }
 }
