ec1157edbc1d0c20b2755a062da837cdf5393aa2
Brock Allen <brockallen@gmail.com>
2021-07-16 13:43:54 -0400

    update EF host for razor pages

---
+++ hosts/EntityFramework/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,9 @@
+
+namespace IdentityServerHost.Pages.Logout
+{
+    public class LogoutOptions
+    {
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+    }
+}
