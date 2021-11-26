04ec2fa95e31a49dae75702d8e76fd1873ad53e6
Brock Allen <brockallen@gmail.com>
2021-11-26 09:46:02 -0500

    check active

--- src/IdentityServer/Test/TestBackchannelLoginUserValidator.cs
+++ src/IdentityServer/Test/TestBackchannelLoginUserValidator.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 user = _testUserStore.FindBySubjectId(userValidatorContext.IdTokenHintClaims.SingleOrDefault(x => x.Type == JwtClaimTypes.Subject)?.Value);
             }
 
-            if (user != null)
+            if (user != null && user.IsActive)
             {
                 var claims = new List<Claim>
                 {
