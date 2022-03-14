3c7a0fd640000c85be8307980b9798489359fc11
Brock Allen <brockallen@gmail.com>
2022-03-14 12:49:58 -0400

    update migrations for indices

--- migrations/IdentityServerDb/Migrations/PersistedGrantDb/20220314164923_Grants.Designer.cs
+++ migrations/IdentityServerDb/Migrations/PersistedGrantDb/20220314164923_Grants.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityServerDb.Migrations.PersistedGrantDb
 {
     [DbContext(typeof(PersistedGrantDbContext))]
-    [Migration("20220309202509_Grants")]
+    [Migration("20220314164923_Grants")]
     partial class Grants
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -221,8 +221,16 @@ namespace IdentityServerDb.Migrations.PersistedGrantDb
 
                     b.HasKey("Id");
 
+                    b.HasIndex("DisplayName");
+
+                    b.HasIndex("Expires");
+
                     b.HasIndex("Key")
                         .IsUnique();
+
+                    b.HasIndex("SessionId");
+
+                    b.HasIndex("SubjectId");
 
                     b.ToTable("ServerSideSessions", (string)null);
                 });
