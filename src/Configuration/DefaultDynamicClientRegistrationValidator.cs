76ab4f5f51561a7e8eff466750789757b0e91b58
Dominick Baier <dbaier@leastprivilege.com>
2022-08-06 14:04:48 +0200

    add some basic validation

---
+++ src/Configuration/DefaultDynamicClientRegistrationValidator.cs
@@ -0,0 +1,123 @@
+using System.Formats.Asn1;
+using System.Security.Claims;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace Duende.IdentityServer.Configuration;
+
+public class DefaultDynamicClientRegistrationValidator : IDynamicClientRegistrationValidator
+{
+    public Task<DynamicClientRegistrationValidationResult> ValidateAsync(ClaimsPrincipal caller, DynamicClientRegistrationDocument request)
+    {
+        var client = new Client
+        {
+            ClientId = Guid.NewGuid().ToString()
+        };
+        
+        //////////////////////////////
+        // validate grant types
+        //////////////////////////////
+        if (request.GrantTypes.Contains(OidcConstants.GrantTypes.ClientCredentials))
+        {
+            client.AllowedGrantTypes.Add(GrantType.ClientCredentials);
+        }
+        if (request.GrantTypes.Contains(OidcConstants.GrantTypes.AuthorizationCode))
+        {
+            client.AllowedGrantTypes.Add(GrantType.AuthorizationCode);
+        }
+
+        // we only support the two above grant types
+        if (client.AllowedGrantTypes.Count == 0)
+        {
+            return Task.FromResult(new DynamicClientRegistrationValidationResult("unsupported grant type"));
+        }
+
+        if (request.GrantTypes.Contains(OidcConstants.GrantTypes.RefreshToken))
+        {
+            if (client.AllowedGrantTypes.Count == 1 &&
+                client.AllowedGrantTypes.FirstOrDefault(t => t.Equals(GrantType.ClientCredentials)) != null)
+            {
+                return Task.FromResult(new DynamicClientRegistrationValidationResult("client credentials does not support refresh tokens"));
+            }
+
+            client.AllowOfflineAccess = true;
+        }
+        
+        //////////////////////////////
+        // validate redirect URIs
+        //////////////////////////////
+        if (client.AllowedGrantTypes.Contains(GrantType.AuthorizationCode))
+        {
+            if (request.RedirectUris.Any())
+            {
+                foreach (var requestRedirectUri in request.RedirectUris)
+                {
+                    if (requestRedirectUri.IsAbsoluteUri)
+                    {
+                        client.RedirectUris.Add(requestRedirectUri.AbsoluteUri);    
+                    }
+                    else
+                    {
+                        return Task.FromResult(new DynamicClientRegistrationValidationResult("invalid redirect URI"));
+                    }
+                }
+            }
+            else
+            {
+                return Task.FromResult(new DynamicClientRegistrationValidationResult("redirect URI required for authorization_code grant type"));
+            }
+        }
+
+        if (client.AllowedGrantTypes.Count == 1 &&
+            client.AllowedGrantTypes.FirstOrDefault(t => t.Equals(GrantType.ClientCredentials)) != null)
+        {
+            if (request.RedirectUris.Any())
+            {
+                return Task.FromResult(new DynamicClientRegistrationValidationResult("redirect URI not compatible with client_credentials grant type"));
+            }
+        }
+        
+        //////////////////////////////
+        // validate scopes
+        //////////////////////////////
+        if (string.IsNullOrEmpty(request.Scope))
+        {
+            // todo: what to do when scopes are missing? error - leave up to custom validator?
+        }
+        else
+        {
+            var scopes = request.Scope.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            // todo: ideal scope names get checked against configuration store?
+            
+            foreach (var scope in scopes)
+            {
+                client.AllowedScopes.Add(scope);
+            }
+        }
+        
+        //////////////////////////////
+        // secret handling
+        //////////////////////////////
+        
+        // todo: if jwks is present - convert JWKs into secrets
+        // todo: otherwise generate shared secret (set a default lifetime?)
+        // todo: add jwks_uri suppport
+        
+        
+        //////////////////////////////
+        // misc
+        //////////////////////////////
+        if (!string.IsNullOrWhiteSpace(request.ClientName))
+        {
+            client.ClientName = request.ClientName;
+        }
+        
+        if (request.ClientUri != null)
+        {
+            client.ClientUri = request.ClientUri.AbsoluteUri;
+        }
+        
+        // validation successful - return client
+        return Task.FromResult(new DynamicClientRegistrationValidationResult(client)); 
+    }
+}
--- src/Configuration/DynamicClientRegistrationValidationResult.cs
+++ src/Configuration/DynamicClientRegistrationValidationResult.cs
@@ -4,5 +4,23 @@ namespace Duende.IdentityServer.Configuration;
 
 public class DynamicClientRegistrationValidationResult
 {
-    public Client Client { get; set; }
+    public DynamicClientRegistrationValidationResult(Client client)
+    {
+        ArgumentNullException.ThrowIfNull(client);
+
+        Client = client;
+    }
+
+    public DynamicClientRegistrationValidationResult(string error)
+    {
+        ArgumentNullException.ThrowIfNull(error);
+
+        Error = error;
+    }
+    
+    public Client? Client { get; }
+
+    public string? Error { get; }
+
+    public bool IsError => !string.IsNullOrWhiteSpace(Error);
 }
