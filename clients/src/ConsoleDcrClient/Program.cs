46d71644b5636739a3c97c34c5acf6f71fc8720d
Dominick Baier <dbaier@leastprivilege.com>
2022-08-14 16:45:50 +0200

    add test client

---
+++ clients/src/ConsoleDcrClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using Clients;
+using IdentityModel.Client;
+
+var client = new HttpClient();
+
+var document = new DynamicClientRegistrationDocument
+{
+    GrantTypes = { "client_credentials" },
+    Scope = "api1"
+};
+
+var response = await client.RegisterClientAsync(new DynamicClientRegistrationRequest
+{
+    Address = Constants.Authority + "/connect/dcr",
+    Document = document
+});
+
+if (response.IsError)
+{
+    Console.WriteLine(response.Error);
+    return;
+}
+
+Console.WriteLine(JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true }));
