0b52c26dadbc4773a0b0b8720357a03c8069e185
Brock Allen <brockallen@gmail.com>
2023-04-05 16:48:09 -0400

    nullable ISigningKeyStore

--- src/Storage/Models/SerializedKey.cs
+++ src/Storage/Models/SerializedKey.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+#nullable enable
+
 using System;
 
 namespace Duende.IdentityServer.Models;
@@ -14,21 +16,21 @@ public class SerializedKey
     /// Version number of serialized key.
     /// </summary>
     public int Version { get; set; }
-        
+
     /// <summary>
     /// Key identifier.
     /// </summary>
-    public string Id { get; set; }
+    public string Id { get; set; } = default!;
 
     /// <summary>
     /// Date key was created.
     /// </summary>
     public DateTime Created { get; set; }
-        
+
     /// <summary>
     /// The algorithm.
     /// </summary>
-    public string Algorithm { get; set; }
+    public string Algorithm { get; set; } = default!;
 
     /// <summary>
     /// Contains X509 certificate.
@@ -38,8 +40,8 @@ public class SerializedKey
     /// <summary>
     /// Serialized data for key.
     /// </summary>
-    public string Data { get; set; }
-        
+    public string Data { get; set; } = default!;
+
     /// <summary>
     /// Indicates if data is protected.
     /// </summary>
