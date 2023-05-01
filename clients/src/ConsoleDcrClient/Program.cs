7f1db864eb46c94c9b11721ad50dd328383d27a3
Joe DeCock <josephdecock@gmail.com>
2023-04-30 22:04:19 -0500

    Update console dcr client

--- clients/src/ConsoleDcrClient/Program.cs
+++ clients/src/ConsoleDcrClient/Program.cs
@@ -4,47 +4,76 @@ using System.Text.Json;
 using Clients;
 using IdentityModel.Client;
 
-var client = new HttpClient();
 
-var document = new DynamicClientRegistrationDocument
-{
-    GrantTypes = { "client_credentials" },
-    Scope = "api1"
-};
 
-var response = await client.RegisterClientAsync(new DynamicClientRegistrationRequest
-{
-    Address = Constants.Authority + "/connect/dcr",
-    Document = document
-});
-
-if (response.IsError)
-{
-    Console.WriteLine(response.Error);
-    return;
-}
-
-Console.WriteLine(JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true }));
+Console.Title = "Dynamic Client Registration - Client Credentials Flow";
+await RegisterClient();
+Console.ReadLine();
+var response = await RequestTokenAsync();
+response.Show();
 
 Console.ReadLine();
+await CallServiceAsync(response.AccessToken);
+Console.ReadLine();
 
-document = new DynamicClientRegistrationDocument
+static async Task RegisterClient()
 {
-    GrantTypes = { "client_credentials", "authorization_code", "refresh_token" },
-    RedirectUris = { new Uri("https://client/cb") },
-    Scope = "api1"
-};
+    var client = new HttpClient();
 
-response = await client.RegisterClientAsync(new DynamicClientRegistrationRequest
-{
-    Address = Constants.Authority + "/connect/dcr",
-    Document = document
-});
+    var request = new DynamicClientRegistrationRequest
+    {
+        Address = Constants.Authority + "/connect/dcr",
+        Document = new DynamicClientRegistrationDocument
+        {
 
-if (response.IsError)
-{
-    Console.WriteLine(response.Error);
-    return;
+            GrantTypes = { "client_credentials" },
+            Scope = "resource1.scope1 resource2.scope1 IdentityServerApi"
+        }
+    };
+    request.Document.Extensions.Add("client_id", "client");
+    request.Document.Extensions.Add("client_secret", "secret");
+
+    var response = await client.RegisterClientAsync(request);
+
+    if (response.IsError)
+    {
+        Console.WriteLine(response.Error);
+        return;
+    }
+    Console.WriteLine(JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true }));
 }
 
-Console.WriteLine(JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true }));
+static async Task<TokenResponse> RequestTokenAsync()
+{
+    var client = new HttpClient();
+
+    var disco = await client.GetDiscoveryDocumentAsync(Constants.Authority);
+    if (disco.IsError) throw new Exception(disco.Error);
+
+    var response = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+    {
+        Address = disco.TokenEndpoint,
+
+        ClientId = "client",
+        ClientSecret = "secret",
+    });
+
+    if (response.IsError) throw new Exception(response.Error);
+    return response;
+}
+
+static async Task CallServiceAsync(string token)
+{
+    var baseAddress = Constants.SampleApi;
+
+    var client = new HttpClient
+    {
+        BaseAddress = new Uri(baseAddress)
+    };
+
+    client.SetBearerToken(token);
+    var response = await client.GetStringAsync("identity");
+
+    "\n\nService claims:".ConsoleGreen();
+    Console.WriteLine(response.PrettyPrintJson());
+}
