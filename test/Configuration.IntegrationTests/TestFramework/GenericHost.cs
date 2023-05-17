e3a64e0e327d8c0a2649de8cc01cb9432299b5e2
Joe DeCock <josephdecock@gmail.com>
2023-05-17 09:33:59 -0500

    Revert generic host change that seems to break

--- test/Configuration.IntegrationTests/TestFramework/GenericHost.cs
+++ test/Configuration.IntegrationTests/TestFramework/GenericHost.cs
@@ -79,7 +79,7 @@ public class GenericHost
     {
         services.AddLogging(options =>
         {
-            options.SetMinimumLevel(LogLevel.Critical);
+            options.SetMinimumLevel(LogLevel.Debug);
             options.AddProvider(Logger);
         });
 
