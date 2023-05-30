a3efdd9923d5038facbffcff49d4c64b32dbb1d1
Brock Allen <brockallen@gmail.com>
2023-05-30 14:17:47 -0400

    only emit warnings once, and add product wide warning

--- src/IdentityServer/Licensing/LicenseValidator.cs
+++ src/IdentityServer/Licensing/LicenseValidator.cs
@@ -81,6 +81,7 @@ internal partial class LicenseValidator
 
             // we're not using our _warningLog because we always want this emitted regardless of the context
             _logger.LogWarning(message);
+            WarnForProductFeatures();
             return;
         }
 
