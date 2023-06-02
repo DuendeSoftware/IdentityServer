0f9bf1a04239a5cd0230abfc744adfb3faadd651
Joe DeCock <josephdecock@gmail.com>
2023-06-02 09:39:45 -0500

    Prevent null AuthenticationScheme names on login

--- hosts/main/Pages/Account/Login/Index.cshtml.cs
+++ hosts/main/Pages/Account/Login/Index.cshtml.cs
@@ -175,7 +175,7 @@ public class Index : PageModel
 
             if (!local)
             {
-                View.ExternalProviders = new[] { new ViewModel.ExternalProvider { AuthenticationScheme = context.IdP } };
+                View.ExternalProviders = new[] { new ViewModel.ExternalProvider ( authenticationScheme: context.IdP ) };
             }
 
             return;
@@ -186,19 +186,19 @@ public class Index : PageModel
         var providers = schemes
             .Where(x => x.DisplayName != null)
             .Select(x => new ViewModel.ExternalProvider
-            {
-                DisplayName = x.DisplayName ?? x.Name,
-                AuthenticationScheme = x.Name
-            }).ToList();
+            (
+                authenticationScheme: x.Name,
+                displayName: x.DisplayName ?? x.Name
+            )).ToList();
 
-        var dyanmicSchemes = (await _identityProviderStore.GetAllSchemeNamesAsync())
+        var dynamicSchemes = (await _identityProviderStore.GetAllSchemeNamesAsync())
             .Where(x => x.Enabled)
             .Select(x => new ViewModel.ExternalProvider
-            {
-                AuthenticationScheme = x.Scheme,
-                DisplayName = x.DisplayName ?? x.Scheme
-            });
-        providers.AddRange(dyanmicSchemes);
+            (
+                authenticationScheme: x.Scheme,
+                displayName: x.DisplayName ?? x.Scheme
+            ));
+        providers.AddRange(dynamicSchemes);
 
 
         var allowLocal = true;
@@ -208,9 +208,7 @@ public class Index : PageModel
             allowLocal = client.EnableLocalLogin;
             if (client.IdentityProviderRestrictions != null && client.IdentityProviderRestrictions.Any())
             {
-                providers = providers.Where(provider => 
-                    provider.AuthenticationScheme != null &&
-                    client.IdentityProviderRestrictions.Contains(provider.AuthenticationScheme)).ToList();
+                providers = providers.Where(provider => client.IdentityProviderRestrictions.Contains(provider.AuthenticationScheme)).ToList();
             }
         }
 
--- hosts/main/Pages/Account/Login/ViewModel.cs
+++ hosts/main/Pages/Account/Login/ViewModel.cs
@@ -16,7 +16,13 @@ public class ViewModel
         
     public class ExternalProvider
     {
+        public ExternalProvider(string authenticationScheme, string? displayName = null)
+        {
+            AuthenticationScheme = authenticationScheme;
+            DisplayName = displayName;
+        }
+
         public string? DisplayName { get; set; }
-        public string? AuthenticationScheme { get; set; }
+        public string AuthenticationScheme { get; set; }
     }
 }
