cdd0056eef5ef6e10a0056c4da976c70933a2434
Joe DeCock <josephdecock@gmail.com>
2023-06-02 10:37:32 -0500

    Copy fixes to EF host

--- hosts/EntityFramework/Pages/Ciba/InputModel.cs
+++ hosts/EntityFramework/Pages/Ciba/InputModel.cs
@@ -6,7 +6,7 @@ namespace IdentityServerHost.Pages.Ciba;
 public class InputModel
 {
     public string? Button { get; set; }
-    public IEnumerable<string> ScopesConsented { get; set; } = Enumerable.Empty<string>();
+    public IEnumerable<string> ScopesConsented { get; set; } = new List<string>();
     public string? Id { get; set; }
     public string? Description { get; set; }
 }
