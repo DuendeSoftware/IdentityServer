a285ea873f90240e526a3dae72bb1fdeb0508a44
Brock Allen <brockallen@gmail.com>
2023-06-07 14:32:49 -0400

    first stab at designing response generators for  our endpoint result objects

---
+++ src/IdentityServer/Hosting/IEndpointResultGenerator.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Duende.IdentityServer.Hosting;
+
+/// <summary>
+/// Endpoint result generator
+/// </summary>
+public interface IEndpointResultGenerator<in T>
+    where T : IEndpointResult
+{
+    /// <summary>
+    /// Writes the endpoint result to the HTTP response.
+    /// </summary>
+    Task ProcessAsync(T result, HttpContext context);
+}
+
