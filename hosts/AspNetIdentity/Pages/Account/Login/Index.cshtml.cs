f3fb44ee3ea32a359237451328f20674cc6b3f89
Brock Allen <brockallen@gmail.com>
2023-06-21 10:31:31 -0400

    add null-forgiving operator based on model binding checks

--- hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
+++ hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
@@ -97,11 +97,11 @@ public class Index : PageModel
 
         if (ModelState.IsValid)
         {
-            var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberLogin, lockoutOnFailure: true);
+            var result = await _signInManager.PasswordSignInAsync(Input.Username!, Input.Password!, Input.RememberLogin, lockoutOnFailure: true);
             if (result.Succeeded)
             {
-                var user = await _userManager.FindByNameAsync(Input.Username);
-                await _events.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.UserName, clientId: context?.Client.ClientId));
+                var user = await _userManager.FindByNameAsync(Input.Username!);
+                await _events.RaiseAsync(new UserLoginSuccessEvent(user!.UserName, user.Id, user.UserName, clientId: context?.Client.ClientId));
 
                 if (context != null)
                 {
