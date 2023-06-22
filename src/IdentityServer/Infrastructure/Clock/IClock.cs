13c90b1bdebdefb616a7ef79b449cd58dd811c5e
Brock Allen <brockallen@gmail.com>
2023-06-22 10:27:27 -0400

    use TimeProvider in the default clock

--- src/IdentityServer/Infrastructure/Clock/IClock.cs
+++ src/IdentityServer/Infrastructure/Clock/IClock.cs
@@ -15,16 +15,26 @@ public interface IClock
     /// <summary>
     /// The current UTC date/time.
     /// </summary>
-    DateTimeOffset UtcNow 
-    { 
-        get
-        {
-            return DateTimeOffset.UtcNow;
-        }
-    } 
+    DateTimeOffset UtcNow { get; }
 }
 
-class DefaultClock : IClock { }
+class DefaultClock : IClock
+{
+    private readonly TimeProvider _timeProvider;
+
+    public DefaultClock()
+    {
+        _timeProvider = TimeProvider.System;
+    }
+
+    public DefaultClock(TimeProvider timeProvider)
+    {
+        _timeProvider = timeProvider;
+    }
+
+    public DateTimeOffset UtcNow { get => _timeProvider.GetUtcNow(); }
+}
+
 class LegacyClock : IClock
 {
 #pragma warning disable CS0618 // Type or member is obsolete
