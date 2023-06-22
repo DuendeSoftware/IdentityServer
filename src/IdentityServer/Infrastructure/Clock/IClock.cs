43412604ad1fa52cfc753ecc9ae33bd0a0edf76b
Brock Allen <brockallen@gmail.com>
2023-06-22 09:58:48 -0400

    add clock abstraction

---
+++ src/IdentityServer/Infrastructure/Clock/IClock.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Authentication;
+using System;
+
+namespace Duende.IdentityServer;
+
+/// <summary>
+/// Abstraction for the date/time.
+/// </summary>
+public interface IClock
+{
+    /// <summary>
+    /// The current UTC date/time.
+    /// </summary>
+    DateTimeOffset UtcNow 
+    { 
+        get
+        {
+            return DateTimeOffset.UtcNow;
+        }
+    } 
+}
+
+class DefaultClock : IClock { }
+class LegacyClock : IClock
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    private readonly ISystemClock _clock;
+#pragma warning restore CS0618 // Type or member is obsolete
+
+#pragma warning disable CS0618 // Type or member is obsolete
+    public LegacyClock(ISystemClock clock)
+#pragma warning restore CS0618 // Type or member is obsolete
+    {
+        _clock = clock;
+    }
+
+    public DateTimeOffset UtcNow
+    {
+        get => _clock.UtcNow;
+    }
+}
