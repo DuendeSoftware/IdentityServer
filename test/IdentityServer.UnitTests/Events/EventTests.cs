9e3e79876757d87b09cb88f871b76cd5d482543f
Joe DeCock <josephdecock@gmail.com>
2023-07-07 15:58:10 -0500

    Broken test demonstrating bug #1362

---
+++ test/IdentityServer.UnitTests/Events/EventTests.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Duende.IdentityServer.Configuration;
+using Duende.IdentityServer.Endpoints.Results;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.ResponseHandling;
+using Duende.IdentityServer.Validation;
+using FluentAssertions;
+using IdentityModel;
+using UnitTests.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Xunit;
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Events;
+
+namespace UnitTests.Endpoints.Results;
+
+public class EventTests
+{
+
+    [Fact]
+    public void UnhandledExceptionEventCanCallToString()
+    {
+        try
+        {
+            throw new Exception();
+        }
+        catch (Exception ex)
+        {
+            var unhandledExceptionEvent = new UnhandledExceptionEvent(ex);
+
+            var s = unhandledExceptionEvent.ToString();
+
+            s.Should().NotBeNullOrEmpty();
+        }
+    }
+}
