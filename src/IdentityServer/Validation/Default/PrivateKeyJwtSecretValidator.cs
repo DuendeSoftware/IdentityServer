83cf53fd8556847063e7c73e5a1d902b4cb73343
Joe DeCock <josephdecock@gmail.com>
2023-10-07 10:19:33 -0500

    Add PAR as valid aud in private key jwt
    To conform with https://datatracker.ietf.org/doc/html/rfc9126#name-request:
    
    > the authorization server MUST accept its ... pushed authorization
    > request endpoint URL ... as an intended audience.

--- src/IdentityServer/Validation/Default/PrivateKeyJwtSecretValidator.cs
+++ src/IdentityServer/Validation/Default/PrivateKeyJwtSecretValidator.cs
@@ -99,7 +99,9 @@ public class PrivateKeyJwtSecretValidator : ISecretValidator
             await _issuerNameService.GetCurrentAsync(),
             // CIBA endpoint: https://openid.net/specs/openid-client-initiated-backchannel-authentication-core-1_0.html#auth_request
             string.Concat(_urls.BaseUrl.EnsureTrailingSlash(), ProtocolRoutePaths.BackchannelAuthentication),
-            // TODO: PAR once added
+            // PAR endpoint: https://datatracker.ietf.org/doc/html/rfc9126#name-request
+            string.Concat(_urls.BaseUrl.EnsureTrailingSlash(), ProtocolRoutePaths.PushedAuthorization),
+            
         }.Distinct();
 
         var tokenValidationParameters = new TokenValidationParameters
