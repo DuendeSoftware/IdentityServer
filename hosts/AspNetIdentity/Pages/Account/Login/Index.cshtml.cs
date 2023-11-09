073040319f2392f8b5a591647ff882021f77d5cb
HansM <HansM2013@gmx.de>
2023-11-09 10:59:06 +0100

    Small spelling fix in Index.cshtml.cs

--- hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
+++ hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
@@ -182,14 +182,14 @@ public class Index : PageModel
                 AuthenticationScheme = x.Name
             }).ToList();
 
-        var dyanmicSchemes = (await _identityProviderStore.GetAllSchemeNamesAsync())
+        var dynamicSchemes = (await _identityProviderStore.GetAllSchemeNamesAsync())
             .Where(x => x.Enabled)
             .Select(x => new ViewModel.ExternalProvider
             {
                 AuthenticationScheme = x.Scheme,
                 DisplayName = x.DisplayName
             });
-        providers.AddRange(dyanmicSchemes);
+        providers.AddRange(dynamicSchemes);
 
 
         var allowLocal = true;
