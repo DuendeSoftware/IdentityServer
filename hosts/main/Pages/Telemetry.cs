d3da581cabef76377ecf92eef090897474ad1a8d
Brock Allen <brockallen@gmail.com>
2023-12-20 09:21:01 -0500

    add copyrights

--- hosts/main/Pages/Telemetry.cs
+++ hosts/main/Pages/Telemetry.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
 using System.Diagnostics.Metrics;
 
 namespace IdentityServerHost.Pages;
