61ae6f83afcc828aeeb426261d33fb600518aa1e
Petr Waclawek <Pecan@oriflame.com>
2024-05-01 10:33:43 +0200

    Keep old value of max_age when removed

--- src/IdentityServer/Extensions/ValidatedAuthorizeRequestExtensions.cs
+++ src/IdentityServer/Extensions/ValidatedAuthorizeRequestExtensions.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Collections.Specialized;
+using System.Globalization;
 
 #pragma warning disable 1591
 
@@ -47,6 +48,16 @@ public static class ValidatedAuthorizeRequestExtensions
             OidcConstants.PromptModes.SelectAccount,
             OidcConstants.PromptModes.Create
         }).ToArray();
+    }
+
+    public static void RemoveMaxAge(this ValidatedAuthorizeRequest request)
+    {
+        request.Raw.Remove("max_age");
+
+        if (request.MaxAge.HasValue)
+        {
+            request.Raw.Add(Constants.ProcessedMaxAge, request.MaxAge.Value.ToString(CultureInfo.InvariantCulture));
+        }
     }
 
     public static string GetPrefixedAcrValue(this ValidatedAuthorizeRequest request, string prefix)
