11d775659f0cd4420de72fb0d35e009df203a337
Joe DeCock <josephdecock@gmail.com>
2024-09-18 21:26:30 -0500

    Rename ClientModel
    The new System.ClientModel library is a dependency that we get from EF, and its namespace name conflicts with our existing class name. There's no good way to resolve the ambiguity, short of just renaming the model. We need to do the same in the templates. (https://github.com/DuendeSoftware/IdentityServer.Templates/issues/56)

--- hosts/EntityFramework/Pages/Admin/Clients/Edit.cshtml.cs
+++ hosts/EntityFramework/Pages/Admin/Clients/Edit.cshtml.cs
@@ -19,7 +19,7 @@ public class EditModel : PageModel
     }
 
     [BindProperty]
-    public ClientModel InputModel { get; set; } = default!;
+    public EditClientModel InputModel { get; set; } = default!;
     [BindProperty]
     public string? Button { get; set; }
 
