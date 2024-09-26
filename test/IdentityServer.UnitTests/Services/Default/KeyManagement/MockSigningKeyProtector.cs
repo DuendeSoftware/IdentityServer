0c7677dfbacb4f1deb4de2a8ded49f2a525cbc69
Joe DeCock <josephdecock@gmail.com>
2024-09-26 10:34:18 -0500

    Use data protection in the mocking signing key protector

--- test/IdentityServer.UnitTests/Services/Default/KeyManagement/MockSigningKeyProtector.cs
+++ test/IdentityServer.UnitTests/Services/Default/KeyManagement/MockSigningKeyProtector.cs
@@ -4,13 +4,21 @@
 
 using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services.KeyManagement;
+using Microsoft.AspNetCore.DataProtection;
 using System;
 
 namespace UnitTests.Services.Default.KeyManagement;
 
 class MockSigningKeyProtector : ISigningKeyProtector
 {
+    private IDataProtector _dataProtector;
     public bool ProtectWasCalled { get; set; }
+    
+    public MockSigningKeyProtector()
+    {
+        var provider = new EphemeralDataProtectionProvider();
+        _dataProtector = provider.CreateProtector("test");
+    }
 
     public SerializedKey Protect(KeyContainer key)
     {
@@ -21,12 +29,12 @@ class MockSigningKeyProtector : ISigningKeyProtector
             Algorithm = key.Algorithm,
             IsX509Certificate = key.HasX509Certificate,
             Created = DateTime.UtcNow,
-            Data = KeySerializer.Serialize(key),
+            Data = _dataProtector.Protect(KeySerializer.Serialize(key)),
         };
     }
 
     public KeyContainer Unprotect(SerializedKey key)
     {
-        return KeySerializer.Deserialize<RsaKeyContainer>(key.Data);
+        return KeySerializer.Deserialize<RsaKeyContainer>(_dataProtector.Unprotect(key.Data));
     }
 }
